6b63546c7e0b041513a2f292a6aab87ac487e24d
vext01 <vext01@gmail.com>
2012-07-02 21:20:04 +0100

    distinguish min line from the rest

--- oppl_parse.ml
+++ oppl_parse.ml
@@ -1,28 +1,24 @@
 open Str;;
 open BatString;;
+open List;;
 
 (* Eg. "abc" =~ "^a" is true *)
 let (=~) s re = Str.string_match (Str.regexp re) s 0;;
 
-(*
-let parse_line line =
-        print_string ("GOT: '" ^ line ^ "'\n");
-        (
-        if BatString.is_empty line then
-                ()
-        else
-                print_string "not empty\n";
+let parse_real_line line =
+        let elems = Str.split (Str.regexp ":") line in
+        let prefix = (List.hd elems) in
+        match prefix with
+        "min" -> print_string("MIN LINE\n"); ()
+        | _ -> print_string("CONSTRAINT\n"); ();;
 
-        );;
-*)
-
-let parse_line line =
-        print_string ("GOT: '" ^ line ^ "'\n");
-        match line with
-        "" -> print_string "EMPTY\n"; ()
-        | _ -> (if BatString.starts_with line "#" then ()
-                else print_string "REAL_LINE\n"; ()
+let parse_line line = match line with
+        "" -> ()
+        | _ -> (
+                if BatString.starts_with line "#" then ()
+                else parse_real_line line; ()
                );;
+
 let parse filename = 
         let file = open_in filename in try
                 while true do (parse_line (input_line file)) done
