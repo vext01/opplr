3e0a861f711e9c1aa1009b22c0c23b84dbf83d27
vext01 <vext01@gmail.com>
2012-07-04 13:33:34 +0100

    add license, make banner underlined

--- oppl.ml
+++ oppl.ml
@@ -1,3 +1,19 @@
+(*
+ * Copyright (c) 2012), Edd Barrett <vext01@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *)
+
 open Str;;
 open BatString;;
 open BatMap;;
@@ -266,10 +282,11 @@ let sys = {
 
 let oppl_version = "0.1";;
 let oppl_years = "2012";;
-Printf.printf "\nOPPL Version %s\n(C) Edd Barrett %s\n\n" oppl_version oppl_years;;
+Printf.printf "\nOPPL Version %s\n(C) Edd Barrett %s\n" oppl_version oppl_years;;
+print_string "-----------------------------------------\n";;
 
 parse sys get_filename;;
-print_string "\nConstraint system loaded:\n";;
+print_string "Constraint system loaded:\n";;
 Printf.printf "Variables: %d\n" sys.next_var_num;;
 print_string("Objective Func: \n");;
 Printf.printf "Constraints: %d\n" (List.length sys.cstrs);;
